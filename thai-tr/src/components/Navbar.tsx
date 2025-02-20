e34bec9b01e8bd19ed3f90f89da923ed8330e5a2
Alain Bonneau <alaindev97@gmail.com>
2025-02-20 17:58:18 +0100

    add h1

--- thai-tr/src/components/Navbar.tsx
+++ thai-tr/src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 function MyNavbar() {
   return (
-    <header className="bg-customred-500 text-white p-4">
-      <nav className=""></nav>
+    <header className="bg-customred-500 text-white p-6 text-center">
+      <nav className="">
+        <h1>🇹🇭 แอปพลิเคชันเพื่อแปลประเทศไทยเป็นภาษาฝรั่งเศสที่ง่ายขึ้น 🇹🇭 / 🇫🇷 Un moyen plus simple de traduire le thaï en français 🇫🇷</h1>
+      </nav>
     </header>
   );
 }
